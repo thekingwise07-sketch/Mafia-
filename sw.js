38f756a24c414825b464f5113c13924d17c6ba84
thekingwise07-sketch <thekingwise07@gmail.com>
2026-09-23 09:19:45 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'jalsat-satih-v1';
+const ASSETS = [
+  './',
+  'index.html',
+  'mafia.html',
+  'who-is.html',
+  'jeem-seen.html',
+  'manifest.json',
+  'logo.png',
+  'icon-192.png',
+  'icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((resp) => {
+        // cache newly fetched same-origin files for next time offline
+        if (event.request.method === 'GET' && resp && resp.status === 200 && resp.type === 'basic') {
+          const clone = resp.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        }
+        return resp;
+      }).catch(() => cached);
+    })
+  );
+});
